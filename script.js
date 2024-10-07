cacc3845d1f8220b70a8957a019a5355281f08ed
Maryann Tran <99710041+mktran21@users.noreply.github.com>
2024-10-07 16:51:02 -0500

    changed js file and matched file names

--- script.js
+++ script.js
@@ -7,20 +7,50 @@
 // this function outputs user inputted data
 function getData() {
   var formContents = document.getElementById("medicalForm");
-  var formOutput;
+  var formOutput =
+    "<table class='output'><th>Dataname</th><th>Type</th><th>Value</th>";
   var dataType;
 
-  formOutput =
-    "<table class='output'><th>Dataname</th><th>Type</th><th>Value</th>";
-
   for (var i = 0; i < formContents.length; i++) {
-    console.log(
-      "item: " +
-        i +
-        " " +
-        formContents.elements[i].name +
-        " = " +
-        formContents.elements[i].value
-    );
+    dataType = formContents.elements[i].type;
+    // switch case for different types of data
+    switch (dataType) {
+      case "checkbox":
+        if (formContents.elements[i].checked) {
+          formOutput +=
+            "<tr><td align='right'>" + formContents.elements[i].name + "</td>";
+          formOutput += "<td align='right'>" + dataType + "</td>";
+          formOutput += "<td class='outputdata'>Checked</td></tr>";
+        }
+        break;
+      case "radio":
+        if (formContents.elements[i].checked) {
+          formOutput +=
+            "<tr><td align='right'>" + formContents.elements[i].name + "</td>";
+          formOutput += "<td align='right'>" + dataType + "</td>";
+          formOutput +=
+            "<td class='outputdata'>" +
+            formContents.elements[i].value +
+            "</td></tr>";
+        }
+        break;
+      case "button":
+      case "submit":
+      case "reset":
+        break;
+      default:
+        formOutput +=
+          "<tr><td align='right'>" + formContents.elements[i].name + "</td>";
+        formOutput += "<td align='right'>" + dataType + "</td>";
+        formOutput +=
+          "<td class='outputdata'>" +
+          formContents.elements[i].value +
+          "</td></tr>";
+    }
   }
+
+  formOutput += "</table>";
+
+  // Set the form output to the div with id 'outputFormData'
+  document.getElementById("outputFormData").innerHTML = formOutput;
 }
